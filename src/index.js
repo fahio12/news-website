f8c88e58dae90ba4ce26055be677a3959720ff09
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-07-08 13:00:28 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+// TEMPLATE ENGINE
+const handlebars = require('express-handlebars');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'main',
+    partialsDir: __dirname + '/views/partials/'
+}));
+app.use(express.static('public'))
+
+// ROUTES
+var home = require("./routes/home")
+app.use('/',home)
+
+
+
+app.listen(process.env.PORT || 80, function () {
+  console.log("Server started")
+})
